6410aad1cc34e486767d39eed7935965845fa130
Jim Carr <jfcarr@gmail.com>
2019-03-12 20:25:51 -0400

    degrees-to-radians and radians-to-degrees conversions

---
+++ src/convert.go
@@ -0,0 +1,9 @@
+package main
+
+func radiansToDegrees(radians float32) float32 {
+	return (radians * 180) / 3.14159265358979
+}
+
+func degreesToRadians(degrees float32) float32 {
+	return (degrees * 3.14159265358979) / 180
+}
